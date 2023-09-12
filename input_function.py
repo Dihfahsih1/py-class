6040f83c8a51f8f0767fde76312b4674006b8366
Dihfahsih1 <dihfahsihm@gmail.com>
2023-09-12 16:49:38 +0300

    clean up the repo

--- input_function.py
+++ input_function.py
@@ -3,7 +3,7 @@ def input_object():
     religion=input('enter your religion: ')
     status=input('What is your status?: ') 
     school=input('Which school did you go to?: ')
-    age=int(input('Which school did you go to?: '))
+    age=int(input('how old are you?: '))
     
     print(f"my name is {name} my religion is: {religion} and am {status} \n my school is {school} and am {age} years old")
 input_object()
