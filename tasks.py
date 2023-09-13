44f492cb122f4b782517005ce8c2bb3a99c78d59
mugabo22 <ronaldmugabo424@gmail.com>
2023-09-13 17:41:32 +0300

    this code

---
+++ tasks.py
@@ -0,0 +1,30 @@
+def sumup_list():
+    list1=(1,2,3,4,5)
+    sum_up=int(1+2+3+4+5)
+    print(int(sum_up))
+
+sumup_list()
+
+def my_list():
+    list1=["den",290,29.9,"miracle"]
+    list2=[2,3,4,"ben","nes",2.3]
+    list3=["hsa",2.3,4,2]
+    print("list2[3]:", list2[3])
+
+my_list()
+
+def calculate():
+    den=("What Is Your Name")
+    A="sum of all upper case letters in den"
+    B="sum of all lower case letters in den"
+    print("den")
+    print ("the sum of upper is A and the sum for lower is B")
+calculate()
+
+def this_list():
+    list1=[1,2,4,5,6,0]
+    list2=[1,2,2,2,2,3]
+    list.count(list2)
+    print(list1,list2)
+this_list()
+
