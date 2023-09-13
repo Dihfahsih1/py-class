1aa3e5bfa8f5bec4e97dd55d1b33b98c0e7e219c
alvindez <dezwasswa4@gmail.com>
2023-09-14 03:13:36 +0600

    assignment

--- Assignment_One/assignment1.py
+++ Assignment_One/assignment1.py
@@ -1,3 +1,33 @@
+# Task 1
+
 name = "Tessa"
 age = 22
 print(f"My name is {name}, and I am {age} years old")
+
+# Task 2
+
+fruit = ["Apple", "Mango", "rusberries"]
+print(fruit)
+
+# Task 3
+
+student_info = {"name": "Glen", "age": "12", "grade": "6"}
+value = student_info["name", "age", "grade"]
+print(value)
+
+# Task 4
+colors = ("Blue","Red", "White", "Green")
+print(colors)
+
+# Task 5
+def greet(name):
+    print("Hello {name}. You are welcome.")
+greet("Deby")
+
+# Task 6
+def calculte_age(age):
+    year_of_birth = input("Birth year:  ")
+    result = 2023 - year_of_birth
+
+
+    
