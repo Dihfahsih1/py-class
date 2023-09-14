067a751050b7891d9db5b6c02a2c0faa47b87e37
mugabo22 <ronaldmugabo424@gmail.com>
2023-09-14 16:54:58 +0300

    update the dictioneryfunction

--- Assignment1/assignment1.py
+++ Assignment1/assignment1.py
@@ -18,9 +18,28 @@ def fruits_list():
     print(list1)
 fruits_list()
 
-def my_dict():
+my_name="mugabo"
+name=my_name
+print(str(name))
+
+my_age=int(67)
+age=my_age
+print(age)
+
+def my_life():
+    name=input('what is your name:')
+    age=int(input("what is your age:"))
+    
+    print(f'my name is {name} and am {age} years old')
+my_life()
+
+def fruits_list():
+    list1=["mangoes","apples","berrys"]
+    print(list1)
+fruits_list()
+
+
+def dict_func():
     student_info=dict(name="john",age=int(63),grade="first")
-    my_dict=student_info
-    for x in my_dict:
-        print(x)
-my_dict()
+    print(student_info)
+dict_func()
