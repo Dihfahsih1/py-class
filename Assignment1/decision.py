79c436ca43f31c8a4cdfe1979c7f1811fe9ed714
mugabo22 <ronaldmugabo424@gmail.com>
2023-09-15 12:29:19 +0300

    final part

---
+++ Assignment1/decision.py
@@ -0,0 +1,22 @@
+# average=int(input("Whats your average: "))
+# if average >80:
+#     print("excellent")
+# else:print("nice work buddy")
+# if average >60:
+#     print("more effort is needed buddy")
+# else:
+#     print("nice try buddy")
+
+
+
+my_dict={"name":"max","age":27,"status":"single"}
+my_dict["email"]="sams@gmail.com"
+print(my_dict)
+value=my_dict["name"]
+print(value)
+
+       
+   
+    
+    
+    
