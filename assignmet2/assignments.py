e4a44276775d06261e6362ba4f4d31b47570cc9b
mugabo22 <ronaldmugabo424@gmail.com>
2023-09-18 20:48:21 +0300

    updated assignment

--- assignmet2/assignments.py
+++ assignmet2/assignments.py
@@ -1,23 +1,30 @@
-your_age=int(input("enter your age:"))
-if your_age >=18:
-    print("you are older")
-else:
-    print("you are a minor")
-if your_age <=5:
-    print("you are an infant")
+def  my_age():
+     your_age=int(input("enter your age:"))
+     if your_age >=18:
+        print("you are older")
+     else:
+      print("you are a minor")
+     if your_age <=5:
+      print("you are an infant")
+my_age()
 
-numbers=[]
+def numbers(): 
+   numbers=[]
 
-print("enter numbers of your choice")
-number_one=int(input("number one:"))
-number_two=int(input("number two"))
-number_three=int(input("number three"))
+   print("enter numbers of your choice")
+   number_one=int(input("number one: "))
+   number_two=int(input("number two: "))
 
-add=(number_one + number_two + number_three)
-print(add)
-listm=(number_one,number_two,number_three)
-numbers.append(listm)
-print(numbers)
+   number_three=int(input("number three: "))
+   number_four=int(input("number four: "))
+   number_five=int(input("number five: "))
+
+   add=(number_one + number_two + number_three)
+   print(add)
+   listm=(number_one,number_two,number_three)
+   numbers.append(listm)
+   print(numbers)
+numbers()
 
 
 def my_list():
@@ -26,35 +33,37 @@ def my_list():
         print(x)
 my_list()
           
-
-man=[[18,28,56],["men",27,"yuan"],[1,2,3]]
-print(man)
-man.insert(1,[2,3,4,5])
-print(man)
-man[1][2]="dollar"
-print(man)
-print(man[1][2])
-for r in man:
+def my_list():
+  man=[[18,28,56],["men",27,"yuan"],[1,2,3]]
+  print(man)
+  man.insert(1,[2,3,4,5])
+  print(man)
+  man[1][2]="dollar"
+  print(man)
+  print(man[1][2])
+  for r in man:
     for c in r :
         print(c,end = " ")
     print()
+my_list()
     
     
-def square(x=4):
-    return(x*x)
-print(square(4))
+def my_square(x):
+    square=int(x*x)
+    return square
+my_square(6)
 
-def sqaure_and_print(number):
-    print(number)
-    print(pow(8, 2))
-sqaure_and_print(8)
+def sqaure_and_print(maddos):
+    number=int(input("enter your number: "))
+    numbers1=my_square(number)
+    print(numbers1)
+sqaure_and_print('maddos')
     
 
 def greet(name):
-    print(name)
-print("hello","alex")
-print('hello','peter')
-print('hello','mandy')
+    user_name=input("enter your name: ")
+    print(f'how are you {user_name} ')
+greet('user_name')
 
 
   
