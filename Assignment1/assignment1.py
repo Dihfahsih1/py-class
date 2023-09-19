6170d61e107c38f74aaf491ca68059d37e35b506
mugabo22 <ronaldmugabo424@gmail.com>
2023-09-19 18:02:13 +0300

    Tasks

--- Assignment1/assignment1.py
+++ Assignment1/assignment1.py
@@ -1,73 +1,73 @@
-# my_name="mugabo"
-# name=my_name
-# print(str(name))
+my_name="mugabo"
+name=my_name
+print(str(name))
 
-# my_age=int(67)
-# age=my_age
-# print(age)
+my_age=int(67)
+age=my_age
+print(age)
 
-# def my_life():
-#     name=input('what is your name:')
-#     age=int(input("what is your age:"))
+def my_life():
+    name=input('what is your name:')
+    age=int(input("what is your age:"))
     
-#     print(f'my name is {name} and am {age} years old')
-# my_life()
+    print(f'my name is {name} and am {age} years old')
+my_life()
 
-# def fruits_list():
-#     list1=["mangoes","apples","berrys"]
-#     print(list1)
-# fruits_list()
+def fruits_list():
+    list1=["mangoes","apples","berrys"]
+    print(list1)
+fruits_list()
 
-# my_name="mugabo"
-# name=my_name
-# print(str(name))
+my_name="mugabo"
+name=my_name
+print(str(name))
 
-# my_age=int(67)
-# age=my_age
-# print(age)
+my_age=int(67)
+age=my_age
+print(age)
 
-# def my_life():
-#     name=input('what is your name:')
-#     age=int(input("what is your age:"))
+def my_life():
+    name=input('what is your name:')
+    age=int(input("what is your age:"))
     
-#     print(f'my name is {name} and am {age} years old')
-# my_life()
+    print(f'my name is {name} and am {age} years old')
+my_life()
 
-# def fruits_list():
-#     list1=["mangoes","apples","berrys"]
-#     print(list1)
-# fruits_list()
-
-
-# def dict_func():
-#     student_info=dict(name="john",age=int(63),grade="first")
-#     print(student_info)
-# dict_func()
-
-# my_tuple=("yellow","blue","orange")
-# for x in my_tuple:
-#     print(x)
-# print(my_tuple)
-
-# def greet():
-#     name="ronald"
-#     print("how are you",name)
-# greet()
-
-# def calc_age():
-
-#     age=int(input("Enter your age: "))    
-#     print(f'you are {age} years old')
-# calc_age()
+def fruits_list():
+    list1=["mangoes","apples","berrys"]
+    print(list1)
+fruits_list()
 
 
-# average_population=int(input("whats the average population of Uganda:"))
-# if average_population >80:
-#     print("country in danger")
-# elif average_population >=70:
-#     print("country soon in danger ")
-# else:
-#     print("country is safe")
+def dict_func():
+    student_info=dict(name="john",age=int(63),grade="first")
+    print(student_info)
+dict_func()
+
+my_tuple=("yellow","blue","orange")
+for x in my_tuple:
+    print(x)
+print(my_tuple)
+
+def greet():
+    name="ronald"
+    print("how are you",name)
+greet()
+
+def calc_age():
+
+    age=int(input("Enter your age: "))    
+    print(f'you are {age} years old')
+calc_age()
+
+
+average_population=int(input("whats the average population of Uganda:"))
+if average_population >80:
+    print("country in danger")
+elif average_population >=70:
+    print("country soon in danger ")
+else:
+    print("country is safe")
 
 
     
--- Assignment1/decision.py
+++ Assignment1/decision.py
@@ -1,63 +1,42 @@
-average=int(input("Whats your average: "))
-if average >80:
-    print("excellent")
-else:print("nice work buddy")
-if average <60:
-    print("more effort is needed buddy")
-
-my_dict={"name":"max","age":27,"status":"single"}
-my_dict["email"]="sams@gmail.com"
-print(my_dict)
-value=my_dict["name"]
-print(value)
-
-# while loops in python
-s =1 
-while s < 6:
-  s += 2
-  if s == 3:
-          continue
-  print(s)
-
-i = 0
-while i < 6:
-  i += 1
-  if i == 3:
-    continue
-  print(i)
-
-i= 1
-while i < 6:
-  print(i)
-  i += 1
-else:
-  print("i is no longer less than 6")
-
-# Understanding for loops in python
-fruits=['apple','banana','orange']
-for x in fruits:
-  if x=='banana':
-    break
-  print(x)
-
-for x in range(6):
-  if x == 3: break
-  print(x)
-else:
-  print("Finally finished!")
-
-adj = ["red", "big", "tasty"]
-fruits = ["apple", "banana", "cherry"]
-
-for x in adj:
-  for y in fruits:
-    print(x, y)
-
-for x in [1,2,3]:
-  pass
+x=[1,2,3,4]
+for x in x:
+    if x==3:
+        continue
     
-    
+print(x)
+
+def loops():
+    list1=["mengo","man",2,3,4,26.70,56.6]
+    for x in list1:
+        print(x)
+
+loops()
+            
+def ranged():
+     daf=[1,2,3,4]  
+     fat=["man","men","toy"]
+     voe=["nam","meo","jok"]
+     for x in daf:
+          for y in fat:
+               print(x, y)
+     for x in fat:
+          for y in voe:
+               print(x, y)
+ranged()
+
+for x in range(4):
+     for j in range(3):
+          print(f"({x},{j})")
+
+def forloop():
+    my_star=("a developer")
+    while "a developer" in my_star:
+        print('developer exists')
+        
+forloop()
    
+
+        
     
     
     
