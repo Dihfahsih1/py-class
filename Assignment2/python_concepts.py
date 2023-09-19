78ddaa6885b21257278bf714f57c89f62e7c2227
alvindez <dezwasswa4@gmail.com>
2023-09-19 20:51:47 +0600

    a few changes

--- Assignment2/python_concepts.py
+++ Assignment2/python_concepts.py
@@ -1,27 +1,31 @@
 # TASK1. CONDITIONAL STATEMENT CHALLENGE
-m = int(input("Enter your age: "))
+def condition():
+    m = int(input("Enter your age: "))
 
-if m >= 18:
-    print ("You are an adult")
-elif m > 5 < 18:
-    print ("You are a minor")
-elif m <= 5:
-    print ("You are an infant")
-
-else:
-    print ()
+    if m >= 18:
+        print ("You are an adult")
+    elif m > 5 < 18:
+        print ("You are a minor")
+    elif m <= 5:
+        print ("You are an infant")
+condition()
 
 
 # TASK2. LIST MANIPULATION
-numbers = []
-a = int(input("First number: "))
-b = int(input("Second number: "))
-c = int(input("Third number: "))
-d = int(input("Fourth number: "))
-e = int(input("Fifth number: "))
-sum = (a + b + c + d + e)
-print(sum)
-
+def add():
+    numbers = []
+    a = int(input("First number: "))
+    b = int(input("Second number: "))
+    c = int(input("Third number: "))
+    d = int(input("Fourth number: "))
+    e = int(input("Fifth number: "))
+    f = (a,b,c,d,e)
+    numbers.append(f)
+    print (numbers)
+    sum = (a + b + c + d + e)
+    print(sum)
+    
+add()
 
 # TASK4. LISTS AND ITERATION
 colors = ["red", "blue", "green", "yellow", "purple"]
@@ -30,30 +34,29 @@ for a in colors:
 
 def my_colors():
     colors = [["red", "blue", "green"], ["yellow", "Math", "purple"]]
-    for a in range(len(colors)):
-        for b in range(len(colors[a])):
-            print("Index of", colors[a][b], "is", (a, b))
+    for a in enumerate(colors, 100):
+        print(a)
+    for count, a in enumerate(colors, 100):
+        print(count, a)
+    for count, a in enumerate(colors):
+        print(count)
+        print(a)
 my_colors()
 
 
 # TASK5. CALLING A FUNCTION WITHIN A FUNCTION
-def square():
-    m = int(input("Enter number: "))
-    result = m*m
-    return result
-def square_and_print():
-    m = int(input("Enter number: "))
-    result = m*m
-    print (f"The square of {m} is {result}")
-    return result
-    square()
-square_and_print()
+def square(number):
+    return number*number
+def square_and_print(b):
+    return (square(b))
+number = int(input("Enter number: "))
+print(f"The square of{number} is {square(b)}")
     
 
 # TASK6. PARAMETERS AND ARGUMENTS
-def greet():
-    name = input("Enter your name: ")
-    print (f"Hello {name}, how are you?")
-greet()
+def greet(name):
+    return name 
+name = input("Enter your name: ")
+print(f"Hello {name}, you are welcome.")
 
 
