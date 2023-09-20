b4310cf5b4c5c71a4aa90bcdfbceabddefb3369c
alvindez <dezwasswa4@gmail.com>
2023-09-20 10:16:16 +0600

    made visible

--- alvinfor_loop.py
+++ alvinfor_loop.py
@@ -1,25 +1,25 @@
-# def loops():
-#     colors = ["red", 2, True]
-#     for a in colors:
-#         print(a) 
-# loops()
+def loops():
+    colors = ["red", 2, True]
+    for a in colors:
+        print(a) 
+loops()
 
-# def ran():
-#     for a in range(1,6,3):
-#         print(a) 
-# ran()
-# # LOOP BACKWARD
-# def ran():
-#     for a in range(6,0,-1):
-#         print(a) 
-# ran()
+def ran():
+    for a in range(1,6,3):
+        print(a) 
+ran()
+# LOOP BACKWARD
+def ran():
+    for a in range(6,0,-1):
+        print(a) 
+ran()
 
-# def ran():
-#     for a in list(range(6,0,-1)):
-#         print(a) 
-# ran()
+def ran():
+    for a in list(range(6,0,-1)):
+        print(a) 
+ran()
 
-# NESTED LOOPS
+NESTED LOOPS
 def nested():
     for a in range(4):
         for b in range(5):
