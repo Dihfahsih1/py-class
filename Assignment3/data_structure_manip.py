29ae33b6374da033fce63c733d431ac0ea1c92b4
alvindez <dezwasswa4@gmail.com>
2023-09-25 17:15:15 +0600

    assignment changes

--- Assignment3/data_structure_manip.py
+++ Assignment3/data_structure_manip.py
@@ -1,1 +1,18 @@
-student_grades = []
+def grades():
+    student_grades = []
+    a = ("Enter student name: ")
+    b = ("Enter student name: ")
+    c = ("Enter student name: ")
+    d = ("Enter student name: ")
+    e = ("Enter student name: ")
+    f = (a,b,c d e)
+    f.append(student_grades)
+    print(student_grades)
+
+    for m in student_grades():
+        average = sum(f) / len(f)
+        print(average)
+
+    student_info = {
+        studeent_name : student_grades
+    }
